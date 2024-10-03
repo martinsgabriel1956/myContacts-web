a1a4ee8a486af5fe2293e1661eeb55d1f9296209
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-10-03 12:12:43 -0300

    feat: loading contact data in the edit contact page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import { ContactForm } from '../../components/ContactForm';
 import { PageHeader } from '../../components/PageHeader';
+import ContactsService from '../../services/ContactsService';
+import { Loader } from '../../components/Loader';
+import { toast } from '../../utils/toast';
 
 export function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          params.id,
+        );
+
+        console.log(contactData);
+        setIsLoading(false);
+      } catch (error) {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [params.id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar contato" />
       <ContactForm
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
